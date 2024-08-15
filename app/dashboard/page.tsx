d610b4a7a60a6343e1970f6ac2358156f2185a18
habib-hb <developerhabib1230@gmail.com>
2024-08-15 16:42:00 +0600

    huge success :)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -15,7 +15,7 @@ const Dashboard = () => {
 
                 <a href="/comments" className="text-2xl bg-slate-600 text-white rounded-lg p-8 m-2 cursor-pointer hover:scale-125 w-[90vw] md:w-[60vw] text-center shadow-xl">Comments</a>
 
-                <a href="" className="text-2xl bg-slate-600 text-white rounded-lg p-8 m-2 cursor-pointer hover:scale-125 w-[90vw] md:w-[60vw] text-center shadow-xl">Customers</a>
+                <a href="/customers" className="text-2xl bg-slate-600 text-white rounded-lg p-8 m-2 cursor-pointer hover:scale-125 w-[90vw] md:w-[60vw] text-center shadow-xl">Customers</a>
 
                 <a href="" className="text-2xl bg-slate-600 text-white rounded-lg p-8 m-2 cursor-pointer hover:scale-125 w-[90vw] md:w-[60vw] text-center shadow-xl">Admins</a>
 

22730033ad651f50a03fb2d51ef2e778e115364e
habib-hb <developerhabib1230@gmail.com>
2024-08-20 17:10:12 +0600

    awesome

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,20 +7,85 @@ const Dashboard = () => {
 
             <div className="flex flex-col items-center justify-center">
 
-                <a href="/dashboard/product-upload" className="text-2xl bg-slate-600 text-white rounded-lg p-8 m-2 cursor-pointer hover:scale-125 w-[90vw] md:w-[60vw] text-center shadow-xl">Product Upload</a>
+                <a href="/dashboard/product-upload" className="text-2xl text-white rounded-lg  cursor-pointer  w-[90vw] md:w-[60vw] text-center ">
+                <div className="flex flex-col bg-slate-600 justify-center items-center my-4 p-2  shadow-lg hover:scale-125 rounded-lg">
 
-                <a href="/product_edit" className="text-2xl bg-slate-600 text-white rounded-lg p-8 m-2 cursor-pointer hover:scale-125 w-[90vw] md:w-[60vw] text-center shadow-xl">Product Edit</a>
+                        <img src="/images_icons/product_upload.png" className="h-14 w-14"/>
 
-                <a href="/product_delete" className="text-2xl bg-slate-600 text-white rounded-lg p-8 m-2 cursor-pointer hover:scale-125 w-[90vw] md:w-[60vw] text-center shadow-xl">Product Delete</a>
+                        <p>Product Upload</p>
+                
+                </div>
+                </a>
 
-                <a href="/comments" className="text-2xl bg-slate-600 text-white rounded-lg p-8 m-2 cursor-pointer hover:scale-125 w-[90vw] md:w-[60vw] text-center shadow-xl">Comments</a>
+                <a href="/product_edit" className="text-2xl  text-white rounded-lg  cursor-pointer  w-[90vw] md:w-[60vw] text-center ">
+                <div className="flex flex-col bg-slate-600 justify-center items-center my-4 p-2  shadow-lg hover:scale-125 rounded-lg">
 
-                <a href="/customers" className="text-2xl bg-slate-600 text-white rounded-lg p-8 m-2 cursor-pointer hover:scale-125 w-[90vw] md:w-[60vw] text-center shadow-xl">Customers</a>
+                        <img src="/images_icons/product_edit.png" className="h-14 w-14"/>
 
-                <a href="/admins" className="text-2xl bg-slate-600 text-white rounded-lg p-8 m-2 cursor-pointer hover:scale-125 w-[90vw] md:w-[60vw] text-center shadow-xl">Admins</a>
+                        <p>Product Edit</p>
+                
+                </div>
+                </a>
 
-                <a href="/orders" className="text-2xl bg-slate-600 text-white rounded-lg p-8 m-2 cursor-pointer hover:scale-125 w-[90vw] md:w-[60vw] text-center shadow-xl">Orders</a>
-               
+                <a href="/product_delete" className="text-2xl  text-white rounded-lg  cursor-pointer  w-[90vw] md:w-[60vw] text-center ">
+                <div className="flex flex-col bg-slate-600 justify-center items-center my-4 p-2  shadow-lg hover:scale-125 rounded-lg">
+
+                        <img src="/images_icons/product_delete.png" className="h-14 w-14"/>
+
+                        <p>Product Delete</p>
+                
+                </div>
+                </a>
+
+                <a href="/comments" className="text-2xl  text-white rounded-lg  cursor-pointer  w-[90vw] md:w-[60vw] text-center ">
+                <div className="flex flex-col bg-slate-600 justify-center items-center my-4 p-2  shadow-lg hover:scale-125 rounded-lg">
+
+                        <img src="/images_icons/product_comments.png" className="h-14 w-14"/>
+
+                        <p>Comments</p>
+                
+                </div>
+                </a>
+
+                <a href="/customers" className="text-2xl  text-white rounded-lg  cursor-pointer  w-[90vw] md:w-[60vw] text-center ">
+                <div className="flex flex-col bg-slate-600 justify-center items-center my-4 p-2  shadow-lg hover:scale-125 rounded-lg">
+
+                        <img src="/images_icons/product_customers.png" className="h-14 w-14"/>
+
+                        <p>Customers</p>
+                
+                </div>
+                </a>
+
+                <a href="/admins" className="text-2xl  text-white rounded-lg  cursor-pointer  w-[90vw] md:w-[60vw] text-center ">
+                <div className="flex flex-col bg-slate-600 justify-center items-center my-4 p-2  shadow-lg hover:scale-125 rounded-lg">
+
+                        <img src="/images_icons/product_admins.png" className="h-14 w-14"/>
+
+                        <p>Admins</p>
+                
+                </div>
+                </a>
+
+                <a href="/orders" className="text-2xl  text-white rounded-lg  cursor-pointer  w-[90vw] md:w-[60vw] text-center ">
+                <div className="flex flex-col bg-slate-600 justify-center items-center my-4 p-2  shadow-lg hover:scale-125 rounded-lg">
+
+                        <img src="/images_icons/product_orders.png" className="h-14 w-14"/>
+
+                        <p>Orders</p>
+                
+                </div>
+                </a>
+
+                <a href="/orders" className="text-2xl  text-white rounded-lg  cursor-pointer  w-[90vw] md:w-[60vw] text-center ">
+                <div className="flex flex-col bg-slate-600 justify-center items-center my-4 p-2  shadow-lg hover:scale-125 rounded-lg">
+
+                        <img src="/images_icons/product_banner.png" className="h-14 w-14"/>
+
+                        <p>Banner Upload</p>
+
+                </div>
+                </a>
                 
             </div>
         </Container>
